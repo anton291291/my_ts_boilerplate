781e6ff23a3222392f7b4cb140b585408c648142
Anton Sokolov <anton291291@gmail.com>
2020-04-19 00:16:16 +0300

    add webpack, eslint, tsconfig, babel

---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,70 @@
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    mode: 'production',
+    devtool: false,
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        extensions: ['.ts', '.tsx', '.js', 'jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                    /* 'sass-loader', 
+                    'postcss-loader' */
+                ],
+                sideEffects: true
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: 'vendors',
+                    test: /node_modules/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        },
+        namedChunks: true,
+        mangleWasmImports: true,
+        removeAvailableModules: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            })
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
